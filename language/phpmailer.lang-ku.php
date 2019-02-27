61867b57f0f6802cb77fedab899dac6d0fb462e1
Xêro Mîrza <info@xeromirza.com>
2019-02-27 11:46:50 +0100

    Create phpmailer.lang-ku.php
    Language translation for Kurdish.

---
+++ language/phpmailer.lang-ku.php
@@ -0,0 +1,21 @@
+<?php
+//Kurdish (Kurmanci)
+$PHPMAILER_LANG['authenticate']         = 'Çewtîya SMTP : Danişîn venebû.';
+$PHPMAILER_LANG['connect_host']         = 'Çewtîya SMTP: Nebestiya Rajekera SMTP .';
+$PHPMAILER_LANG['data_not_accepted']    = 'Çewtîya SMTP : Dane nehate pejirandin.';
+$PHPMAILER_LANG['empty_message']        = 'Naveroka peyamê vala ye.';
+$PHPMAILER_LANG['encoding']             = 'Kodandina karekterê ya nenas: ';
+$PHPMAILER_LANG['execute']              = 'Nehat xebitandin: ';
+$PHPMAILER_LANG['file_access']          = 'Negiha pelgeyê : ';
+$PHPMAILER_LANG['file_open']            = 'Çewtiya Pelgeyê: Pelge venebû: ';
+$PHPMAILER_LANG['from_failed']          = 'Şandina navnîşanên diyarkirî  pêk nehat: ';
+$PHPMAILER_LANG['instantiate']          = 'Mînaka pêkariya epeyamê nehat pêkanîn.';
+$PHPMAILER_LANG['invalid_address']      = 'Epeyama ne derbasdar: ';
+$PHPMAILER_LANG['mailer_not_supported'] = 'Pirtûkxaneya epeyamê nahê pesartin.';
+$PHPMAILER_LANG['provide_address']      = 'Divê kêmtirîn navnîşana epeyamê ya yekê binivîsî. ';
+$PHPMAILER_LANG['recipients_failed']    = 'Çewtîya SMTP: Wergirên diyarkirî nehatin dîtin: ';
+$PHPMAILER_LANG['signing']              = 'Çewtîya îmzedanê: ';
+$PHPMAILER_LANG['smtp_connect_failed']  = 'Pêkarîya SMTP connect() pêk nehat.';
+$PHPMAILER_LANG['smtp_error']           = 'Çewtîya Rajekerî ya SMTP : ';
+$PHPMAILER_LANG['variable_set']         = 'Guherend nehat sazkirin an  sifirkirin: ';
+$PHPMAILER_LANG['extension_missing']    = 'Pêvek nehate dîtin: ';
